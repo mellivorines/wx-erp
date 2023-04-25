a978541e355e2e53df0a18cccee6e42fc375013e
mellivorines <68333104+mellivorines@users.noreply.github.com>
2023-04-25 08:33:55 +0800

    Update Meta.kt

--- wx-erp-springboot/src/main/kotlin/io/github/mellivorines/wxerp/entity/Meta.kt
+++ wx-erp-springboot/src/main/kotlin/io/github/mellivorines/wxerp/entity/Meta.kt
@@ -30,27 +30,36 @@ interface Meta {
     val title: String?
 
     /**
-     *  允许链接 */
+     *  链接 */
+    @Column(name = "enable_link")
     val enableLink: String?
 
     /**
-     *  允许隐藏 */
+     *  是否隐藏 */
+    @Column(name = "enable_hide")
     val enableHide: String?
 
     /**
-     *  允许填充 */
+     *  是否填充 */
+    @Column(name = "enable_full")
     val enableFull: String?
 
     /**
-     *  enableAffix
+     *  enable_affix
      */
+    @Column(name = "enable_affix")
     val enableAffix: String?
 
     /**
-     *  允许保持 */
+     *  是否激活 */
+    @Column(name = "enable_keep_alive")
     val enableKeepAlive: String?
 
     /**
      *  激活菜单 */
+    @Column(name = "active_menu")
     val activeMenu: String?
+
+    @LogicalDeleted(value = "1", restoredValue = "0")
+    val deleted: Int
 }
